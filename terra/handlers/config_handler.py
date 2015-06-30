9ffce46c025e69bae64cdad9784a23cfb085b72b
Schneider Werner-Walter <noreply@schnwalter.eu>
2015-06-30 20:09:13 +0300

    Update ConfigHandler.

--- terra/handlers/config_handler.py
+++ terra/handlers/config_handler.py
@@ -24,6 +24,7 @@ class ConfigHandler(dict):
         """
         super(ConfigHandler, self).__init__(*args, **kw)
 
+        # Set the default configuration.
         if config_defaults:
             # Store defaults for later usage.
             self.defaults = config_defaults.copy()
@@ -33,62 +34,71 @@ class ConfigHandler(dict):
                 # Add all default sections.
                 self[section_name] = {}
 
+                # Set defaults for each section.
                 for option, value in self.defaults[section_name].iteritems():
                     self[section_name].setdefault(option, value)
 
+        # Use the main config file, in no file path was provided.
         if config_file == '':
             config_file = os.path.expanduser('~/.config/terra/main.yaml')
         self.file = config_file
 
         # Load the user config.
         try:
-            if os.path.exists(config_file):
-                print('[DEBUG] Reading config file: {}'.format(config_file))
+            if os.path.exists(self.file):
+                print('[DEBUG] Reading config file: {}'.format(self.file))
 
                 with open(self.file, 'r') as config_file:
-                    # Load the config file.
-                    user_config = yaml.load(config_file)
+                    # Read the config file contents and create a dictionary.
+                    config_data = yaml.load(config_file)
                     """:type: dict"""
 
-                if user_config:
-                    self.__parse_user_config(user_config)
-
-                # print('[DEBUG] User config loaded:')
-                # print(yaml.dump(user_config, default_flow_style=False, indent=2))
+                    if config_data:
+                        self.__parse_config_data(config_data)
             else:
-                print('[DEBUG] No config file: {}'.format(config_file))
+                print('[DEBUG] No config file: {}'.format(self.file))
 
         except IOError:
-            sys.exit('[ERROR] Could not read config file: {}'.format(config_file))
+            sys.exit('[ERROR] Could not read config file: {}'.format(self.file))
         except yaml.YAMLError:
-            sys.exit('[ERROR] Malformed config file: {}'.format(config_file))
+            sys.exit('[ERROR] Malformed config file: {}'.format(self.file))
 
-    def __parse_user_config(self, user_config):
+    def __parse_config_data(self, config_data):
         """
-        Parse config loaded from a user's config YAML file and merge them with
-        the default config values.
+        Parse the config data loaded from the provided config file.
 
-        :type user_config: dict
+        :type config_data: dict
         """
-        for section_name in user_config:
-            for option, value in user_config[section_name].iteritems():
+        for section_name in config_data:
+            for option, value in config_data[section_name].iteritems():
+                # Allow the deletion of default values.
                 if value is None:
                     del self[section_name][option]
-                else:
-                    if section_name not in self:
-                        self[section_name] = {}
-                    self[section_name][option] = value
+                    continue
+
+                # Create section if it does not exit.
+                if section_name not in self:
+                    self[section_name] = {}
+
+                # Set the option.
+                self[section_name][option] = value
 
     def save(self):
         """
-        Save the active configuration to the user config file.
+        Save the active configuration to the config file.
         """
+
+        # Make sure the config directory exists.
         directory = os.path.dirname(self.file)
         if not os.path.exists(directory):
             os.mkdir(directory)
 
-        with open(self.file, 'wb') as config_file:
-            config_data = self.copy()
+        try:
+            # Open the file and save the config data.
+            with open(self.file, 'wb') as config_file:
+                config_data = self.copy()
 
-            # Save the configuration to file.
-            yaml.dump(config_data, config_file, default_flow_style=False, indent=2)
+                # Save the configuration to file.
+                yaml.dump(config_data, config_file, default_flow_style=False, indent=2)
+        except IOError:
+            print('Could not save the config file: {}'.format(self.file))
